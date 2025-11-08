8e9d928e1ddf72c900ad5264656cfbf3b0f10d0f
Vit Borovsky <vitek.borovsky@gmail.com>
2025-11-08 19:22:04 +0100

    UDP server starting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
+use std::net::SocketAddr;
+use trust_dns_proto::op::Message;
+use trust_dns_proto::serialize::binary::BinDecodable;
+
 mod server;
 
+fn example_callback(data: &[u8], src: SocketAddr) {
+    println!("Processing packet from {:?}", src);
+    match Message::from_bytes(data) {
+        Ok(dns_message) => {
+            println!("Decoded DNS Packet: {:?}", dns_message);
+            // In a real application, you would process the DNS query and send a response
+        },
+        Err(e) => {
+            eprintln!("Error decoding DNS packet: {}", e);
+        }
+    }
+}
+
 fn main() -> std::io::Result<()> {
-    server::start_udp_server()
+    let addr: SocketAddr = "127.0.0.1:5353".parse().expect("Invalid address");
+    server::start_udp_server(addr, example_callback)?;
+
+    Ok(())
 }
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,29 +1,18 @@
-use std::net::UdpSocket;
+use std::net::{UdpSocket, SocketAddr};
 use std::io;
-use trust_dns_proto::op::Message;
-use trust_dns_proto::serialize::binary::BinDecodable;
 
-pub fn start_udp_server() -> io::Result<()> {
-    let socket = UdpSocket::bind("127.0.0.1:5353")?;
-    println!("UDP server listening on 127.0.0.1:5353");
+pub fn start_udp_server<F>(addr: SocketAddr, mut callback: F) -> io::Result<()>
+where
+    F: FnMut(&[u8], SocketAddr),
+{
+    let socket = UdpSocket::bind(addr)?;
+    println!("UDP server listening on {}", addr);
 
     let mut buf = [0; 512]; // Standard DNS UDP packet size
 
     loop {
         let (amt, src) = socket.recv_from(&mut buf)?;
         let received_data = &buf[..amt];
-
-        println!("Received {} bytes from {}", amt, src);
-
-        match Message::from_bytes(received_data) {
-            Ok(dns_message) => {
-                println!("Decoded DNS Packet: {:?}", dns_message);
-                // In a real application, you would process the DNS query and send a response
-            },
-            Err(e) => {
-                eprintln!("Error decoding DNS packet: {}", e);
-            }
-        }
+        callback(received_data, src);
     }
 }
-
