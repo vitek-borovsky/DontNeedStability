9c702d8a90f15646b473465ee2f2ca5d73ed9b04
Vit Borovsky <vitek.borovsky@gmail.com>
2025-11-22 17:21:02 +0100

    added server.rs docstring tests

--- src/server.rs
+++ src/server.rs
@@ -14,6 +14,22 @@ pub struct Server {
 }
 
 impl Server {
+    /// Creates a new `Server` instance, binding to the specified socket address.
+    ///
+    /// # Arguments
+    ///
+    /// * `socket_addr` - The `SocketAddr` to bind the server to.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use std::net::{Ipv4Addr, SocketAddr};
+    /// use dont_need_stability::server::Server;
+    ///
+    /// let socket_addr = SocketAddr::new(Ipv4Addr::new(127, 0, 0, 1).into(), 8080);
+    /// let server = Server::new(socket_addr);
+    /// // Server is created but not yet running.
+    /// ```
     pub fn new(socket_addr: SocketAddr) -> Self {
         Server {
             socket: UdpSocket::bind(socket_addr).unwrap(),
@@ -23,10 +39,72 @@ impl Server {
         }
     }
 
+    /// Registers a callback function to be executed when a UDP packet is received.
+    ///
+    /// The callback function takes the received data as a byte slice and the source `SocketAddr`.
+    /// This method can only be called once before `run()`.
+    ///
+    /// # Arguments
+    ///
+    /// * `f` - A `Box` containing the callback function.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `run()` has already been called.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use std::net::{Ipv4Addr, SocketAddr};
+    /// use dont_need_stability::server::Server;
+    ///
+    /// let socket_addr = SocketAddr::new(Ipv4Addr::new(127, 0, 0, 1).into(), 8081);
+    /// let mut server = Server::new(socket_addr);
+    ///
+    /// server.register_callback(Box::new(|data, src| {
+    ///     println!("Received {:?} bytes from {}", data.len(), src);
+    /// }));
+    /// // Callback is registered, server can now be run.
+    /// ```
     pub fn register_callback(&mut self, f: Box<F>) {
         self.callback = Some(f);
     }
 
+    /// Starts the server, listening for incoming UDP packets and executing the registered callback.
+    ///
+    /// This method spawns a new thread to handle packet reception. It will panic if no callback
+    /// has been registered or if `run()` is called multiple times.
+    ///
+    /// # Panics
+    ///
+    /// * If no callback has been registered using `register_callback()`.
+    /// * If `run()` is called more than once on the same server instance.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use std::net::{Ipv4Addr, SocketAddr, UdpSocket};
+    /// use std::thread;
+    /// use std::time::Duration;
+    /// use dont_need_stability::server::Server;
+    ///
+    /// let server_addr = SocketAddr::new(Ipv4Addr::new(127, 0, 0, 1).into(), 8082);
+    /// let client_addr = SocketAddr::new(Ipv4Addr::new(127, 0, 0, 1).into(), 8083);
+    ///
+    /// let mut server = Server::new(server_addr);
+    /// server.register_callback(Box::new(|data, src| {
+    ///     assert_eq!(data, b"hello");
+    ///     println!("Server received: {:?} from {}", String::from_utf8_lossy(data), src);
+    /// }));
+    /// server.run();
+    ///
+    /// // Simulate a client sending a packet
+    /// let client_socket = UdpSocket::bind(client_addr).unwrap();
+    /// client_socket.send_to(b"hello", server_addr).unwrap();
+    ///
+    /// thread::sleep(Duration::from_millis(100)); // Give server time to process
+    /// server.stop();
+    /// ```
     pub fn run(&mut self) {
         if self.running.load(Ordering::SeqCst) {
             return;
@@ -64,6 +142,25 @@ impl Server {
         self.thread_handle = Some(thread_handle);
     }
 
+    /// Stops the server, joining the background thread.
+    ///
+    /// This method sets an internal flag to signal the background thread to stop,
+    /// then waits for the thread to finish its execution.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use std::net::{Ipv4Addr, SocketAddr};
+    /// use dont_need_stability::server::Server;
+    ///
+    /// let socket_addr = SocketAddr::new(Ipv4Addr::new(127, 0, 0, 1).into(), 8084);
+    /// let mut server = Server::new(socket_addr);
+    /// server.register_callback(Box::new(|_, _| { /* do nothing */ }));
+    /// server.run();
+    /// // ... server is running ...
+    /// server.stop();
+    /// // Server is now stopped.
+    /// ```
     pub fn stop(&mut self) {
         self.running.store(false, Ordering::SeqCst);
         if let Some(handle) = self.thread_handle.take() {
